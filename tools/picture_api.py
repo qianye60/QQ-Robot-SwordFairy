5ec723408c0d7981e1bf23f6d681a7a5ae8bca31
千夜 <1538548527@qq.com>
2024-12-23 01:19:01 +0800

    modify picture_api (#2)
    * Update picture_api.py
    
    * Update config-tools.toml.example

--- tools/picture_api.py
+++ tools/picture_api.py
@@ -1,15 +1,17 @@
 # tools/picture_api.py
 import requests
 from langchain_core.tools import tool
+from .config import config
 import os
 
+picture = config.get("picture_api", {})
+api = picture.get("api")
 
 @tool
-def picture_api(api: str="https://picture.netqianye.com/api.php?type=",type: str="动漫"):
-  """请求随机图片api,保存图片,并且返回图片链接。
+def picture_api(select_type: str):
+  """根据请求从变量api_type_list选择合适的图片分类请求随机图片api，将图片存储到服务器并且返回图片链接。
   Args:
-      api: 请求图片所需的api，需要和type元拼接然后请求。
-      type: 根据用户选择请求图片的类型，类型有"动漫":动漫图片之类的、"风景":大自然风景图之类，如果不存在就使用默认类型。
+    select_type:优先根据用户请求匹配图片类型，例如“美好图片”将匹配"美好"，"动漫图片“将匹配"动漫"，"射射图片"将匹配"射射"。
   """
   try:
     # 获取当前脚本的绝对路径
@@ -17,18 +19,14 @@ def picture_api(api: str="https://picture.netqianye.com/api.php?type=",type: str
     # 获取上级目录的路径
     parent_dir = os.path.dirname(current_dir)
     #请求url
-    pic_response = requests.get(api)
+    pic_response = requests.get(api+select_type)
     if pic_response:
-      pic_type = pic_response.url.split('.')[-1]  # 获取后缀
-      pic_response = requests.get(api+type)
+      pic_type = pic_response.url.split('.')[-1]
       print(pic_response.url)
       with open(parent_dir+"/temp_server/random."+pic_type, 'wb') as f:
         f.write(pic_response.content)
       return pic_response.url
-    else:
-      return None
   except Exception as e:
     print(f"picture_api出现错误: {e}")
     return None
-
 tools = [picture_api]
